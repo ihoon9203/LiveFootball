ab3cc1091c5aee79c32234a52fcf5412fa925d4d
Jeonghoon Oh <ihoon9203@gmail.com>
2022-07-26 16:34:19 -0600

    included constants

---
+++ LiveFootball/Constants.swift
@@ -0,0 +1,33 @@
+//
+//  Constants.swift
+//  LiveFootball
+//
+//  Created by Jeonghoon Oh on 7/26/22.
+//
+
+import Foundation
+
+class Constants {
+    static let baseURL = "https://api-football-v1.p.rapidapi.com/v3/"
+    
+    static let standingEndpoint = "standings"
+    static let countryEndpoint = "countries"
+    static let leagueEndpoint = "leagues"
+    static let teamsEndpoint = "teams"
+    static let playersEndpoint = "players"
+    static let playerBySquadEndpoint = "players/Squad"
+    static let predictionEndpoint = "prediction"
+    static let hthEndpoint = "fixture/headtohead"
+    
+    static let paramCountryCode = "code="
+    static let paramSeason = "season="
+    static let paramLeagueCode = "league="
+    static let paramTeamCode = "team="
+    static let paramDateFrom = "from="
+    static let paramDateTo = "to="
+    static let paramFixtureID = "id="
+    static let paramPlayerID = "id="
+    
+    static let apiKey = "&apiKey="
+    static let key = "726f66a6c7mshc08a20e5b68b867p136a3cjsn787b6c5ccf4b"
+}
